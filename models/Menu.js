112887ebd6182a5502e4b13b98a02aa9a05ef2c3
Thomas Bogle <thomasbogle@Thomass-MacBook-Pro.local>
2023-02-16 11:30:16 +0000

    Connected to db via sequelize and setup Menu & Restaurant models

--- models/Menu.js
+++ models/Menu.js
@@ -2,5 +2,8 @@ const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Menu model
+let Menu = sequelize.define('menu', {
+    title: Sequelize.STRING
+});
 
 module.exports = {Menu};
--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,5 +2,10 @@ const {sequelize} = require('../db');
 const { Sequelize } = require('sequelize');
 
 // TODO - create a Restaurant model
+let Restaurant = sequelize.define('restaurant', {
+    name: Sequelize.STRING,
+    location: Sequelize.STRING,
+    cuisine: Sequelize.STRING
+})
 
 module.exports = {Restaurant};
